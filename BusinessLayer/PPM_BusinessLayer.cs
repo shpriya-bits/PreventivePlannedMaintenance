7b5b78eb9b5e476c0b33f021027ef7ccc69bc912
shpriya-bits <2020mt93084@wilp.bits-pilani.ac.in>
2022-04-02 22:46:16 +0530

    Intital Commit

---
+++ BusinessLayer/PPM_BusinessLayer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace BusinessLayer
+{
+    public class PPM_BusinessLayer
+    {
+        public DataTable List(string section)
+        {
+            using(SqlConnection con = new SqlConnection())
+            {
+                con.ConnectionString = ConfigurationManager.ConnectionStrings["PPM"].ConnectionString;
+                con.Open();
+                SqlCommand cmd = new SqlCommand();
+                if (section == "Eqpt")
+                    cmd.CommandText = "sp_eqpt_list";
+                else if (section == "PPM")
+                    cmd.CommandText = "sp_ppm_list";
+                else if (section == "Defect")
+                    cmd.CommandText = "sp_def_list";
+                else if (section == "PPMS")
+                    cmd.CommandText = "sp_ppms_list";
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Connection = con;
+                SqlDataAdapter sda = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                sda.Fill(dt);
+                return dt;
+            }
+        }
+    }
+}
---
+++ DataLayer/Models/EquipmentMaster.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Models
+{
+    public class EquipmentMaster
+    {
+        public string EquipmentPartId { get; set; }
+        public string Description { get; set; }
+        public bool IsParent { get; set; }
+        public string ParentEquipmentPartId { get; set; }
+        public string Compartment { get; set; }
+        public Nullable<bool> IsPhysical { get; set; }
+        public string CreatedBy { get; set; }
+        public Nullable<System.DateTime> CreatedTs { get; set; }
+        public string ChangedBy { get; set; }
+        public Nullable<System.DateTime> ChangedTs { get; set; }
+                
+        public virtual ICollection<Defect> Defect { get; set; }
+        
+        public virtual ICollection<EquipmentMaster> EquipmentMaster1 { get; set; }
+        public virtual EquipmentMaster EquipmentMaster2 { get; set; }
+        public virtual ICollection<PlannedMaintenance> PlannedMaintenance { get; set; }
+    }
+}
+
